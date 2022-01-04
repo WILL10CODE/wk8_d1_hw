50595e97cdba278785a52ab44144f9374c7d9865
WILL10CODE <wblumenthal100@gmail.com>
2022-01-04 17:25:03 -0600

    Will's Hw

---
+++ src/components/Navbar.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <div className="container-fluid">
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <a className="navbar-brand" href="/">Navbar</a>
+                        <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li className="nav-item">
+                                    <a className="nav-link active" aria-current="page" href="/">Home</a>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/contact">Contact</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/about">About</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/products">Products</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link disabled">Disabled</Link>
+                                </li>
+                            </ul>
+                            <form className="d-flex">
+                                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                                <button className="btn btn-outline-success" type="submit">Search</button>
+                            </form>
+                        </div>
+                    </div>
+                </nav>
+
+            </div>
+        )
+    }
+}
+
---
+++ src/views/Products.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Products extends Component {
+    render() {
+        return (
+            <div>
+                <h2>All I see are Products</h2>
+            
+                
+            </div>
+        )
+    }
+}
